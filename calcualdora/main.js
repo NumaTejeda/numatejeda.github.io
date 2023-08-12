66800f73e77d01d9ffc0438155612153bc2ff8e4
Nico <at.psicotrisquel@gmail.com>
2023-08-12 14:32:15 -0300

    calculadora termianda (falta el readme)

--- calcualdora/main.js
+++ calcualdora/main.js
@@ -4,6 +4,20 @@ let resta = (num1, num2) => {return num1 - num2};
 let division = (num1, num2) => {return num1 / num2};
 let multiplicacion = (num1, num2) => {return num1 * num2};
 
+// Funcion que muestra el resultado 
+function mostrarResultado(resultado){
+    let strResultado = resultado.toString();    //Convierto el resultado tipo number a string para poder buscar con expresiones regulares
+    const buscar = /[-+]/;                      // se busca signo mas (+) y menos (-) que son los que aparecen en la notacion cientifica
+    let busqueda = buscar.test(strResultado);   // utilizo el metodo test que devuelve booleano
+    if(busqueda){
+        return document.getElementById("resultado").innerHTML =  " ERROR! El número contiene demasiados dígitos para ser mostrado.",
+               document.getElementById("resultado").style=  "color: red";
+    }
+    else{
+        return document.getElementById("resultado").innerHTML =  resultado,
+               document.getElementById("resultado").style=  "color: black";
+    }
+}
 //Funcion del boton calcular: aca utilizo las funciones de arriba segun que valor este indicado
 // en el <select>. Tambien manejo el ERROR de operar entre minimo un valor no numerico.
 function calcular(){
@@ -11,11 +25,12 @@ function calcular(){
     let num2 = Number(document.getElementById("number2").value);
     let valorSelect = document.getElementById("operacion").value;  //Guardo los valores en variables
     let resultado = "";
+   
     if(isNaN(num1) || isNaN(num2)){  // Manejo de error
         return document.getElementById("resultado").innerHTML = "ERROR! Verifica los campos ingresados.",
                document.getElementById("resultado").style.color = "red";
     }
-    else if(num2 === 0 && valorSelect ==="division"){
+    else if(num2 === 0 && valorSelect ==="division"){ //Manejo de error
         return document.getElementById("resultado").innerHTML = "ERROR! No se puede dividir por cero.",
                document.getElementById("resultado").style.color = "red";
     }
@@ -34,9 +49,9 @@ function calcular(){
                 resultado = multiplicacion(num1, num2);
                 break;
         }
-        return document.getElementById("resultado").innerHTML = resultado,
-        document.getElementById("resultado").style=  "color: black"; // devulevo resultado dentro del <p> con id "resultado".
+        mostrarResultado(resultado);
     }
+    
 }
 // Funcion de boton reste: restauros los valores pode defecto. 
 function resetValue(){
@@ -60,6 +75,7 @@ function catchErrorNum1(){
 //Igual que el anterior pero para el segundo input.
 function catchErrorNum2(){
     let num2 = Number(document.getElementById("number2").value);
+    let valorSelect = document.getElementById("operacion").value;
     if(isNaN(num2)){
         return document.getElementById("errorNum2").style.display = "block";
     }
@@ -67,4 +83,3 @@ function catchErrorNum2(){
         return document.getElementById("errorNum2").style.display = "none";
     }
 }
-
---
+++ calcualdora/read-me.md
@@ -0,0 +1,1 @@
+# Nicolas Leonardo Numa Tejeda #
